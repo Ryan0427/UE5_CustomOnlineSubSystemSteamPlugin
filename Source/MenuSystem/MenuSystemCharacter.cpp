ab3bca081c2d169cccaedac7a7530a2835464619
ivxxvii <ryan14075@gmail.com>
2025-06-22 23:26:59 +0800

    UE5 Online Subsystem Steam Plugin Build 15

--- Source/MenuSystem/MenuSystemCharacter.cpp
+++ Source/MenuSystem/MenuSystemCharacter.cpp
@@ -66,7 +66,7 @@ AMenuSystemCharacter::AMenuSystemCharacter():
 	{
 		OnlineSessionInterface = OnlineSubsystem->GetSessionInterface();
 
-		if (GEngine)
+		/*if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(
 				-1,
@@ -74,7 +74,7 @@ AMenuSystemCharacter::AMenuSystemCharacter():
 				FColor::Blue,
 				FString::Printf(TEXT("Found subsystem %s"), *OnlineSubsystem->GetSubsystemName().ToString())
 			);
-		}
+		}*/
 	}
 }
 
